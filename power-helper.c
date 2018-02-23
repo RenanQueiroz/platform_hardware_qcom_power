b36cbd4676372cc96103d197d3b499ee379578a0
Chris <chris@cypheros.co>
2018-02-23 05:16:47 -0500

    Implementation for Off-Screen Gestures [2/4]
    @Sirhc95  Port from device/qcom/common, adjusted for the QTI powerHAL
    
    Change-Id: I9b7ccfa5705a7e8de7c9b061af74d0e9f9203d0c

--- power-helper.c
+++ power-helper.c
@@ -556,15 +556,6 @@ void __attribute__((weak)) set_device_specific_feature(feature_t UNUSED(feature)
 
 void set_feature(feature_t feature, int state)
 {
-    switch (feature) {
-#ifdef TAP_TO_WAKE_NODE
-        case POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
-            sysfs_write(TAP_TO_WAKE_NODE, state ? "1" : "0");
-            break;
-#endif
-        default:
-            break;
-    }
     set_device_specific_feature(feature, state);
 }
 
